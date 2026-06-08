c13c82aaa9e1958209416a3457a557efecd5df01
Chirag Bajaj <chirag.bajaj.1010@gmail.com>
2026-06-08 15:01:28 +0530

    Model changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    hostel = models.CharField(max_length=20)
+    room_number = models.CharField(max_length=20)
+
+    is_verified = models.BooleanField(default=False)
+    has_voted = models.BooleanField(default=False)
